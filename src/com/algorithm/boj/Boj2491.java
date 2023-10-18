42e4c206b53b47eb675bff036c9cefb7960c6b11
juhwan <kjhw990218@gmail.com>
2023-10-18 16:33:41 +0900

    feat: 그룹단어체커 풀이 추가

---
+++ src/com/algorithm/boj/Boj2491.java
@@ -0,0 +1,22 @@
+package com.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj2491 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        // 입력받은 글자 넣기
+        String input = reader.readLine();
+        // 바뀐 문자들
+        String[] alphabetArray ={"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+        //
+        for (String alphabet : alphabetArray) {
+            input = input.replace(alphabet, "A");
+        }
+
+        System.out.println(input.length());
+    }
+}
